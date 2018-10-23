019e0eb6893b53df02aea53f5edfdb37c37d7021
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-23 16:28:15 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,15 @@
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
+  def initialize(size, val)
+    @wheel_size = size
+    @wheel_number = val
+  end
 
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
 end
